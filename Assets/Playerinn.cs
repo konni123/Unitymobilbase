f4ce9bc99e8c6f186e66e1cf0ac10d7c51cc32c6
konni123 <konrad.gudmundsson@gmail.com>
2019-09-11 11:41:05 +0000

    Inital
    Inital

---
+++ Assets/Playerinn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerinn : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float hradi = 50f;
+    public float sidewaysForce = 50f;
+
+    //public GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log("leikur byrjaður");
+       // rb.AddForce(0, 0,500);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, -hradi * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        /*eða svona
+       if (rb.position.y < -1f)
+       {
+           gameManager.EndGame();
+       }
+       */
+
+        if (rb.position.y < -1f)
+       {
+           FindObjectOfType<GameManager>().EndGame();
+       }
+    }
+}
---
+++ Assets/Triggerinn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggerinn : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hitti triggerinn");
+        gameManager.EndGame();
+    }
+}
